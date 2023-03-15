a796b076cd4aaa2992fd38a0faf52b6fc89373c7
Владюша <127893607+vvkutanin@users.noreply.github.com>
2023-03-15 15:29:40 +0300

    исправил вывод (#10)
    Co-authored-by: kutan <kutan@LAPTOP-FCK6JRD2>

--- Main_RSKP_Project/Program.cs
+++ Main_RSKP_Project/Program.cs
@@ -11,7 +11,7 @@ namespace Main_RSKP_Project
             Console.WriteLine("EEE");
            string aret = "wew";
             int A = 1;
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worl!");
         }
     }
 }
