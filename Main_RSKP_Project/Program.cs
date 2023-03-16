815db4f3013a099409c3784f2e94c602d399f40c
Egor Shramov <egor.shramov@gmail.com>
2023-03-16 13:36:59 +0300

    #18 Исполняемый код для запуска страницы, без самого файла php

--- Main_RSKP_Project/Program.cs
+++ Main_RSKP_Project/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace Main_RSKP_Project
 {
@@ -6,7 +8,25 @@ namespace Main_RSKP_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("SSSSSSSSSSSSSS");
+            Auth();
+            Console.WriteLine("У тебя получилось!");
+        }
+        static void Auth()
+        {
+            string phpFilePath = "C:\\path\\to\\your\\file.php";
+            string phpExecutablePath = "C:\\path\\to\\your\\php.exe"; // путь к исполняемому файлу PHP
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = phpExecutablePath;
+            startInfo.Arguments = $"-S localhost:8000 -t \"{Path.GetDirectoryName(phpFilePath)}\""; // запускаем локальный сервер на порту 8000 и указываем корневую папку
+            startInfo.UseShellExecute = false;
+
+            Process process = new Process();
+            process.StartInfo = startInfo;
+            process.Start();
+
+            // открываем браузер и переходим на страницу с нашим файлом
+            Process.Start("http://localhost:8000/" + Path.GetFileName(phpFilePath));
         }
     }
 }
