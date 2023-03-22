a21f0cf5ba9ff5662295b3a97ffda5a3c798dc31
Egor Shramov <egor.shramov@gmail.com>
2023-03-22 16:46:33 +0300

    #15 Основная площадка для запуска

---
+++ Wisend.Domain/Enum/Gender.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Wisend.Domain.Enum
+{
+    public enum Gen
+    {
+        [Display(Name = "Не выбрано")]
+        Idk = 0,
+        [Display(Name = "Девушка")]
+        Male = 1,
+        [Display(Name = "Парень")]
+        Female  = 2,
+
+    }
+}
