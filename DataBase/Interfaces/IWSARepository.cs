4b5f0ab8159f50c1c3b51e2201e2223f331e1f72
HakerLamer <60318731+HakerLamer@users.noreply.github.com>
2023-03-22 16:49:06 +0300

    #15 Попытка поднять сайт (#19)
    * #15 Попытка поднять сайт
    
    * *15 - исправил конфликт
    
    * #15 Создание страницы
    
    * #15 Решение проблемы с гитигнором
    
    * #15 Исправления с гит игнором
    
    * #15 Основная площадка для запуска
    
    ---------
    
    Co-authored-by: grisar <artemgrizenko07@mail.ru>

---
+++ DataBase/Interfaces/IWSARepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wisend.Domain.Models;
+
+namespace DataConnect.Interfaces
+{
+    public interface IWSARepository : IBaseRepository<AccountWS>
+    {
+        AccountWS GetAccountById(Guid accountId);
+        //async public Task<List<AccountWS>> Select() 
+        //{
+
+        //    return await;
+        //}
+    }
+}
