d50049d59b4a1c6cf289edbb349f8ae15821ad08
kutan <kutan@LAPTOP-FCK6JRD2>
2023-03-27 13:49:51 +0300

    #27 Добавление скрипта на процедуры

--- Main_RSKP_Project/ScriptsDatabase/27_nakat_BD.sql
+++ Main_RSKP_Project/ScriptsDatabase/27_nakat_BD.sql
@@ -9,6 +9,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[Table_users](
+	[ID] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
 	[Login] [nvarchar](50) NOT NULL,
 	[Mail] [nvarchar](50) NOT NULL,
 	[Phone_number] [nvarchar](50) NOT NULL,
@@ -18,6 +19,88 @@ CREATE TABLE [dbo].[Table_users](
 ) ON [PRIMARY]
 GO
 
-INSERT INTO Table_users (Login, Mail, Phone_number, Password, First_date, Last_date) 
-VALUES ('admin', ' project.mail@gmail.com', '+12345678901',  'Qwerty123456', GETDATE(), GETDATE());
+INSERT INTO Table_users VALUES (NEWID(), 'admin', ' project.mail@gmail.com', '+12345678901',  'Qwerty123456', GETDATE(), GETDATE());
+INSERT INTO Table_users VALUES (NEWID(), 'qqwer', ' m12345678900@gmail.com', '+12345678901',  'Qwerty123456', GETDATE(), GETDATE());
+INSERT INTO Table_users VALUES (NEWID(), 'qwert', ' project.mail@gmail.com', '+12345678901',  'Qwerty123456', GETDATE(), GETDATE());
+INSERT INTO Table_users VALUES (NEWID(), 'werty', ' project.mail@gmail.com', '+12345678901',  'Qwerty123456', GETDATE(), GETDATE());
+INSERT INTO Table_users VALUES (NEWID(), 'ertyu', ' project.mail@gmail.com', '+12345678901',  'Qwerty123456', GETDATE(), GETDATE());
+INSERT INTO Table_users VALUES (NEWID(), 'rtyui', ' project.mail@gmail.com', '+12345678901',  'Qwerty123456', GETDATE(), GETDATE());
+GO
+
+SELECT * FROM Table_users
+GO
+
+--Процедура, отвечающая за регистрацию.
+CREATE PROCEDURE CheckAccountExistsReg
+    @Login VARCHAR(50),
+	@Mail VARCHAR(50),
+	@Phone_number VARCHAR(50),
+	@Password VARCHAR(50)
+AS
+BEGIN
+    -- Проверяем, есть ли аккаунт с указанным именем
+    IF EXISTS(SELECT * FROM Table_users WHERE Login = @Login)
+    BEGIN
+        -- Если существует, возвращаем пустой результат
+		SELECT NULL;
+    END
+    ELSE
+    BEGIN
+        -- Если аккаунт не найден, добавляем как нового пользоателя.
+        INSERT INTO Table_users VALUES (NEWID(), @Login, @Mail, @Phone_number, @Password, GETDATE(), GETDATE());
+    END
+END
+GO
+
+-- Процедура, отвечающая за логирование.
+CREATE PROCEDURE CheckAccountExistsLog
+    @accountName VARCHAR(50)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Проверяем, есть ли аккаунт с указанным именем
+    IF EXISTS(SELECT * FROM Table_users WHERE Login = @accountName)
+    BEGIN
+	--Обновляем последнюю дату захода.
+		UPDATE Table_users
+		SET Last_date = GETDATE();
+        -- Возвращаем строку с информацией о найденном аккаунте
+        SELECT * FROM Table_users WHERE Login = @accountName;
+    END
+    ELSE
+    BEGIN
+        -- Если аккаунт не найден, возвращаем пустой результат
+        SELECT NULL;
+    END
+END
+GO
+
+-- Процедура, отвечающая за обновление почты, номера телефона или пароля.
+CREATE PROCEDURE UpdateUserInfo
+    @UserID VARCHAR(50),
+    @Mail VARCHAR(50) = NULL,
+    @Phone_number VARCHAR(20) = NULL,
+    @Password VARCHAR(50) = NULL
+AS
+BEGIN
+    UPDATE Table_users
+    SET
+        Mail = COALESCE(@Mail, Mail),
+        Phone_number = COALESCE(@Phone_number, Phone_number),
+        Password = COALESCE(@Password, Password)
+    WHERE ID = @UserID
+END
+GO
+
+--Процедура для удаления пользователя из базы.
+CREATE PROCEDURE Deleteuser
+	@UserID VARCHAR(50) 
+AS
+BEGIN
+	IF EXISTS(SELECT * FROM Table_users WHERE ID = @UserID)
+	BEGIN
+		DELETE Table_users;
+	END
+END
 GO
